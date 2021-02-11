758283f208cb3b17c314893add0df5b01f2eac22
Sam Tischaefer <samtisch@live.com>
2021-02-11 11:43:16 -0800

    Here it is

--- README.md
+++ README.md
@@ -1,1 +1,35 @@
 # Auto-MC-Config
+
+## WARNING
+I am aware that it only works for 1.15+. If you want to use it on a lower version, you first have to open the instance, then click the "open .minecraft folder" button to generate a .minecraft folder for the script to work with. 
+I have posted an issue on the multimc github: [MultiMC/MultiMC5#3602](https://github.com/MultiMC/MultiMC5/issues/3602)
+
+## What does this do?
+This is a script written in Node.js to automatically disable auto-jump among other things in minecraft
+
+## How do I set this up?
+This script is designed for MultiMC
+
+Prerequisites: 
+
+Step 1: Download code
+
+Step 2: run install.bat or install.sh
+This will run `npm i` to install all the dependencies. You can also use that command directly in a terminal inside the directory of this repository.
+
+Step 3: Open global settings in MultiMC (Not instance settings)
+
+Step 4: Go to "Custom Commands"
+
+Step 5: Put this in "startup script":
+`node "../../../Auto configurator/config.js"`
+<!--Screenshot here--> 
+
+Step 4: The script will now disable/enable specific settings each time you launch an instance. To cusomize those settings, use config.yaml
+
+## FAQ
+- Why did I write this in Node?
+Because I like node
+
+- Can you make this work for the official launcher?
+Maybe. If you find a way to make the default launcher run a script on startup of any installation, I can help you make the script work for it. However, my script depends on environment variables passed by MultiMC.
---
+++ config.js
@@ -0,0 +1,69 @@
+const fs = require('fs')
+var path = require('path')
+const yaml = require('js-yaml')
+
+console.log("Beginning script");
+var instdir = process.env.INST_DIR
+console.log(instdir);
+console.log(process.env.INST_DIR);
+instdir = instdir.replace("\"", '')
+instdir = instdir.replace("\"", '')
+instdir = instdir + '/.minecraft'
+
+async function config() {
+    const config = yaml.load(fs.readFileSync('./../../../Auto-MC-Config/config.yaml', 'utf8'))
+    /*try {
+        fs.mkdirSync(instdir)
+    } catch (error) {
+        console.log("Minecraft dir exists");
+    }*/
+    
+
+    console.log("Instance folder: "+instdir);
+
+    console.log("Working directory:"+__dirname);
+    console.log("Minecraft directory contents:" + fs.readdirSync(instdir));
+    console.log(fs.existsSync(instdir + '/options.txt'));
+    if(!fs.existsSync(instdir + '/options.txt')) {
+        //I learned that minecraft will simply skip over bad options, so I made a options.txt file with the options that may need to be configured.
+        console.log("No options exist. Copying a default options");
+        fs.copyFileSync(path.join(__dirname, './options.txt'), path.join(instdir, './options.txt'))
+        console.log("Default copied. Now writing")
+    } 
+    console.log("Options exist. Reading file")
+    var file = fs.readFileSync(path.join(instdir, 'options.txt'))
+    var string = file.toString()
+
+    console.log("Changing settings");
+    //Auto jump
+    if(string.indexOf('autoJump') > -1 && config.autoJump) {
+        string = string.replace("autoJump:true", 'autoJump:false')
+    }
+    //brightness
+    if(string.indexOf('gamma') > -1 && config.gamma) {
+        string = string.replace(/gamma:.*/, "gamma:1.0")
+    }
+    //music
+    if(string.indexOf('soundCategory_music') > -1 && config.music) {
+        string = string.replace(/soundCategory_music:.*\..*/, "soundCategory_music:0.0")
+    } else if (string.indexOf('music') > -1 && config.music) {
+        //Some older versions have this string instead of the new one
+        string = string.replace(/music:.*\..*/, "music:0.0")
+    }
+    //Multiplayer warning
+    if(string.indexOf('skipMultiplayerWarning') > -1 && config.multiplayer) {
+        string = string.replace("skipMultiplayerWarning:false", 'skipMultiplayerWarning:true')
+    }
+    //Tutorial
+    if(string.indexOf('tutorialStep') > -1 && config.tutorial) {
+        string = string.replace(/tutorialStep.*/, 'tutorialStep:none')
+    }
+    
+
+
+    console.log("Writing file")
+    fs.writeFileSync(path.join(instdir, 'options.txt'), Buffer.from(string, 'utf8'))
+    console.log("File written. Starting instance")
+    
+}
+//config()
